4bb530f42e5cbb81957587b471674678c2ff5537
Danila <AngelofDest1ny@yandex.ru>
2025-02-02 23:21:33 +0300

    feat: implement Spinner

--- src/components/BottomSection/BottomSection.tsx
+++ src/components/BottomSection/BottomSection.tsx
@@ -1,13 +1,14 @@
 import { PureComponent } from 'react';
 import BottomCardList from '../BottomCardList/BottomCardList';
 import { StarshipShortProperties } from '../../api/StarWarsService';
+import { Spinner } from '../Spinner/Spinner';
 interface BottomSectionProp {
   loadingState: boolean;
   data: StarshipShortProperties[];
 }
 class BottomSection extends PureComponent<BottomSectionProp> {
   render() {
-    const { data } = this.props;
+    const { data, loadingState } = this.props;
     return (
       <section>
         <h1>Starships from The Star Wars</h1>
@@ -15,7 +16,7 @@ class BottomSection extends PureComponent<BottomSectionProp> {
           A Starship resource is a single transport craft that has hyperdrive
           capability.
         </h2>
-        <BottomCardList data={data} />
+        {loadingState ? <Spinner /> : <BottomCardList data={data} />}
       </section>
     );
   }
