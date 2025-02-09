1b01bb7c76e3cb2ed2942f69639bd3d594ee10fb
Danila <AngelofDest1ny@yandex.ru>
2025-02-09 13:34:09 +0300

    feat: add react-router and set routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { Routes, Route, Navigate } from 'react-router';
 import './App.scss';
 import SearchPage from './components/SearchPage/SearchPage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
-const App = () => <SearchPage />;
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        element={<Navigate to="/starships/1" replace />}
+        path="/"
+        errorElement={<NotFoundPage />}
+      />
+      <Route
+        element={<Navigate to="/starships/1" replace />}
+        path="/starships"
+        errorElement={<NotFoundPage />}
+      />
+      <Route
+        path="/starships/:id"
+        element={<SearchPage />}
+        errorElement={<NotFoundPage />}
+      >
+        <Route path=":name" errorElement={<NotFoundPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
 
 export default App;
